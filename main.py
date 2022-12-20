48bc457aab74dff3f86808dfc38783bd27e56b7c
legon112 <legon1122006@gmail.com>
2022-12-20 17:16:10 +0200

    HW#23

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import os
-from random import randint
 import sys
 import logging
-from typing import Any, Dict
 import asyncio
+
+from random import randint
+from typing import Any, Dict
 from func_by_Kravchenko import getimage, sub_string
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
-from Tokens import api_token
+from decouple import config
 
 
+API_TOKEN = config('API_TOKEN')
 
 
-API_TOKEN = api_token
-
-# Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
@@ -27,8 +26,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 class Form(StatesGroup):
     answer_human = State()
     sub_string_form = State()
-    
-    
+
 adress = ['answer_hi.txt','answer_whats_up.txt', 'answer_weather.txt','answer_name.txt','answer_age.txt','answer_time.txt']
 bot_answers = {adres : open(mode='r', file = adres, encoding='utf-8').read().split() for adres in adress}
 if not os.path.exists('image'):
@@ -39,28 +37,101 @@ counter_false = 0
 counter_questions = 0
 counter_true = 0
 question = ''
-game_state = False
 
-@dp.message_handler(commands=['start', 'help'])
+
+main_keyboard = types.ReplyKeyboardMarkup()
+math_game_start_keyboard = types.InlineKeyboardMarkup()
+math_game_answers_keyboard = types.InlineKeyboardMarkup()
+math_game_stop_keyboard = types.ReplyKeyboardMarkup()
+sub_string_keyboaed = types.ReplyKeyboardMarkup()
+start_game_button = types.KeyboardButton('/start_game')
+start_sub_string_button = types.KeyboardButton('/start_sub_string')
+math_game_stop_button = types.KeyboardButton('/stop_game')
+sub_string_button = types.KeyboardButton('/start_sub_string')
+type_game_1 = types.InlineKeyboardButton('Выбор решения', callback_data='type_1')
+type_game_2 = types.InlineKeyboardButton('Вписывание ответа', callback_data='type_2')
+main_keyboard.add(start_game_button, start_sub_string_button)
+math_game_start_keyboard.add(type_game_1, type_game_2)
+math_game_stop_keyboard.add(math_game_stop_button)
+sub_string_keyboaed.add(sub_string_button)
+
+
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply("Доступны функции:\nМатематическая игра по команде /start_game\nПри просьбе дать картинку\n(Например:Привет, кинь, пж, картинку кота)\nотправляет картинку\n(если хотите найти картинку по нескольким словам напишите их через подчеркивание)\n(Картинка ищется через pinterest, иногда может не совпадать с темой)\nОтветы на эти вопросы:\nПривет\nКак дела?\nКакая погода за окном?\nКак тебя зовут?\nСколько тебе дней?\nКоторый час?")
+    await message.reply('Привет!\nЯ бот Аби)\nПо команде /help - перечень функция') #, reply_markup=main_keyboard
 
-
+@dp.message_handler(commands=['help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Доступны функции:\nМатематическая игра по команде /start_game\nСожержится ли подстрока в строке /start_sub_string\nПри просьбе дать картинку\n(Например:Привет, кинь, пж, картинку кота)\nотправляет картинку\n(если хотите найти картинку по нескольким словам напишите их через подчеркивание)\n(Картинка ищется через pinterest, иногда может не совпадать с темой)\nОтветы на эти вопросы:\nПривет\nКак дела?\nКакая погода за окном?\nКак тебя зовут?\nСколько тебе дней?\nКоторый час?",)
+    
+    
+    
     
 @dp.message_handler(commands=['start_game'])
 async def math_game_start(message: types.Message):
-    global question, game_state
-    actions = ['-', '+', '*', '/']
-    await message.answer('Game started, answer the question\nFor stoping game write /stop_game')
+    await message.reply('Выберете режим игры', reply_markup=math_game_start_keyboard)
+    
+    
+
+    
+@dp.callback_query_handler()
+async def inline_buttons(query: types.CallbackQuery):
+    global question
+    actions = ['-', '+', '*', '/']  
+    if query.data == 'type_1':
+        false_question = ''
+        number_question = randint(0,3)
+        question = f'{randint(1,10)} {actions[randint(0,3)]} {randint(1,10)}'
+        math_game_answers_keyboard = types.InlineKeyboardMarkup()
+        for i in range(4):
+            if i == number_question:
+                math_game_answers_keyboard.add(types.InlineKeyboardButton(question, callback_data='True'))
+            else:
+                while True:
+                    false_question = f'{randint(1,10)} {actions[randint(0,3)]} {randint(1,10)}'
+                    if eval(false_question) != eval(question):
+                        math_game_answers_keyboard.add(types.InlineKeyboardButton(text = false_question, callback_data='False'))
+                        break
+        await query.message.answer('Game started, answer the question\nFor stoping game write /stop_game', reply_markup=math_game_stop_keyboard)
+        await query.message.answer(eval(question),reply_markup=math_game_answers_keyboard)
+        await Form.answer_human.set()
+    elif query.data == 'type_2':        
+        await query.message.answer('Game started, answer the question\nFor stoping game write /stop_game', reply_markup=math_game_stop_keyboard)
+        question = f'{randint(1,10)} {actions[randint(0,3)]} {randint(1,10)}'
+        await query.message.answer(question)
+        await Form.answer_human.set()
+        
+@dp.callback_query_handler(state=Form.answer_human)
+async def inline_buttons_game(query: types.CallbackQuery):
+    global question, counter_true, counter_false, counter_false, counter_questions
+    if query.data == 'True':
+        counter_true += 1
+        answer_ = 'Yes, next question:\n'
+        counter_questions += 1
+        number_question = randint(0,3)
+    else:
+        counter_false += 1
+        answer_ = f'Wrong, {question}, next question:\n'
+        counter_questions += 1
+    number_question = randint(0,3)
     question = f'{randint(1,10)} {actions[randint(0,3)]} {randint(1,10)}'
-    await message.answer(question)
-    game_state = True
-    await Form.answer_human.set()
+    math_game_answers_keyboard = types.InlineKeyboardMarkup()
+    for i in range(4):
+        if i == number_question:
+            math_game_answers_keyboard.add(types.InlineKeyboardButton(question, callback_data='True'))
+        else:
+            while True:
+                false_question = f'{randint(1,10)} {actions[randint(0,3)]} {randint(1,10)}'
+                if eval(false_question) != eval(question):
+                    math_game_answers_keyboard.add(types.InlineKeyboardButton(text = false_question, callback_data='False'))
+                    break
+    await query.message.answer(answer_ + str(eval(question)), reply_markup=math_game_answers_keyboard)
+    
 
 @dp.message_handler(commands=['stop_game'],state=Form.answer_human)
 async def math_game_stop(message: types.Message, state: FSMContext):
     global counter_true, counter_false, counter_false, counter_questions
-    await message.answer(f'Number of examples: {counter_questions}\nNumber of correct answers: {counter_true}\nNumber of uncorrect answers: {counter_false}')
+    await message.answer(f'Number of examples: {counter_questions}\nNumber of correct answers: {counter_true}\nNumber of uncorrect answers: {counter_false}', reply_markup=types.ReplyKeyboardRemove())
     if counter_true > counter_false:
         await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGv01jkxdn7vbKlIxKhtKhBYUYUNcZoQACCgADnP4yMFsQVnkk-4rwKwQ')
     elif counter_true < counter_false:
@@ -89,19 +160,25 @@ async def math_game_counter(message: types.Message, state: FSMContext):
     await message.answer(answer_ + question)
     
     
-@dp.message_handler(commands=['Start_sub_string'])
+    
+    
+    
+@dp.message_handler(commands=['start_sub_string'])
 async def math_game_start(message: types.Message):
-    await message.reply('Sub string detection is started')
+    await message.reply('Sub string detection is started', reply_markup=sub_string_keyboaed)
     await Form.sub_string_form.set()
 
 @dp.message_handler(commands=['stop_sub_string'],state=Form.sub_string_form)
 async def math_game_stop(message: types.Message, state: FSMContext):
-    await message.reply('Sub string detection is stoped')
+    await message.reply('Sub string detection is stoped',reply_markup=types.ReplyKeyboardRemove())
     await state.finish()
 
 @dp.message_handler(state=Form.sub_string_form)
 async def math_game_counter(message: types.Message, state: FSMContext):
     await message.reply(sub_string(message.text))
+
+
+
 
 
 @dp.message_handler()
@@ -143,5 +220,6 @@ async def echo(message: types.Message):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+    
 
 
