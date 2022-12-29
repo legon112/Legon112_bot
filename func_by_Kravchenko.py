831d2d59f572a832b42697a07274e7c78f5a30f9
legon112 <legon1122006@gmail.com>
2022-12-29 16:42:09 +0200

    HW #25 by Kravchenko

--- func_by_Kravchenko.py
+++ func_by_Kravchenko.py
@@ -9,6 +9,7 @@ from os import path
 def reverse_str(text, first_char, second_char):
     return text[len(text) - second_char - 2 : first_char : -1]
 
+
 def sub_string(str_1 : str) -> bool:
     len_str = len(str_1)
     sub_str = str_1[0]
@@ -39,28 +40,26 @@ def getimage(var):
         
     scrollnum = 1
     sleepTimer = 1
-    # url=f'https://ru.pinterest.com/search/pins/?q={var}&rs=typed'
-    url = f"https://www.google.com/search?q={var}&sxsrf=ALiCzsanLbhpG0jrrDihTGZJPfgm66hF_w:1671009135558&source=lnms&tbm=isch&sa=X&ved=2ahUKEwixx8OP4vj7AhXGi_0HHS2hAaUQ_AUoAXoECAEQAw&biw=958&bih=927&dpr=1"
+    url=f'https://ru.pinterest.com/search/pins/?q={var}&rs=typed'
+    # url = f"https://www.google.com/search?q={var}&sxsrf=ALiCzsanLbhpG0jrrDihTGZJPfgm66hF_w:1671009135558&source=lnms&tbm=isch&sa=X&ved=2ahUKEwixx8OP4vj7AhXGi_0HHS2hAaUQ_AUoAXoECAEQAw&biw=958&bih=927&dpr=1"
     
 
 
-    options=webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches",["enable-logging"])
-    driver=webdriver.Chrome(executable_path='chromedriver.exe', options=options)
+    driver=webdriver.Chrome(executable_path='chromedriver.exe')
     driver.get(url)
-    for i in range(1,scrollnum): 
-        driver.execute_script("window.scrollTo(1,100000)") 
-        print('scroll-down')
-        time.sleep(sleepTimer)
     soup=BeautifulSoup(driver.page_source, 'html.parser')
 
-    it = 0
+    i = 0
     list_links = []
     for link in soup.findAll('img'):   
-        if it == 6:
+        if i == 6:
             break
         namesimage = link.get('src')
         list_links.append(namesimage + '\n')
-        it += 1
+        i += 1
     open(mode='w', file =f'image/{var}.txt', encoding='utf-8').write(''.join(list_links))
     return list_links
+
+def getweather(city : str) -> tuple:
+    main = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=a9186854c80482961a3e3a87bdb23c16&units=metric&lang=ua').json()
+    return main.get('weather')[0].get('description'), str(main.get('main').get('temp'))
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import asyncio
 
 from random import randint
 from typing import Any, Dict
-from func_by_Kravchenko import getimage, sub_string
+from func_by_Kravchenko import getimage, sub_string, getweather
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
@@ -15,7 +15,6 @@ from decouple import config
 
 API_TOKEN = config('API_TOKEN')
 
-
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
@@ -24,8 +23,14 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 class Form(StatesGroup):
+    answer_bot = State()
     answer_human = State()
     sub_string_form = State()
+      
+class Weather(StatesGroup):
+    start = State()
+    location = State()
+    viev = State()
 
 adress = ['answer_hi.txt','answer_whats_up.txt', 'answer_weather.txt','answer_name.txt','answer_age.txt','answer_time.txt']
 bot_answers = {adres : open(mode='r', file = adres, encoding='utf-8').read().split() for adres in adress}
@@ -44,37 +49,51 @@ math_game_start_keyboard = types.InlineKeyboardMarkup()
 math_game_answers_keyboard = types.InlineKeyboardMarkup()
 math_game_stop_keyboard = types.ReplyKeyboardMarkup()
 sub_string_keyboaed = types.ReplyKeyboardMarkup()
+weather_info_keyboard = types.ReplyKeyboardMarkup()
+weather_start_keyboard = types.InlineKeyboardMarkup()
+weather_end_keyboard = types.InlineKeyboardMarkup()
+
+start_button = types.KeyboardButton('/start')
 start_game_button = types.KeyboardButton('/start_game')
 start_sub_string_button = types.KeyboardButton('/start_sub_string')
+start_weather_info_button = types.KeyboardButton('/start_weather_info')
+stop_weather_info_button = types.KeyboardButton('/stop_weather_info')
 math_game_stop_button = types.KeyboardButton('/stop_game')
-sub_string_button = types.KeyboardButton('/start_sub_string')
+sub_string_button = types.KeyboardButton('/stop_sub_string')
 type_game_1 = types.InlineKeyboardButton('Выбор решения', callback_data='type_1')
 type_game_2 = types.InlineKeyboardButton('Вписывание ответа', callback_data='type_2')
-main_keyboard.add(start_game_button, start_sub_string_button)
+dnipro_city = types.InlineKeyboardButton('Дніпро', callback_data='Dnipro')
+kiev_city = types.InlineKeyboardButton('Київ', callback_data='Kiev')
+tours_city = types.InlineKeyboardButton('Тур', callback_data='Tours')
+again_city = types.InlineKeyboardButton('Новый город', callback_data='1')
+new_city = types.InlineKeyboardButton('Повторить погоду', callback_data='0')
+
+main_keyboard.add(start_game_button, start_sub_string_button, start_weather_info_button)
 math_game_start_keyboard.add(type_game_1, type_game_2)
 math_game_stop_keyboard.add(math_game_stop_button)
 sub_string_keyboaed.add(sub_string_button)
+weather_info_keyboard.add(stop_weather_info_button, start_button)
+weather_start_keyboard.add(dnipro_city, kiev_city, tours_city)
+weather_end_keyboard.add(new_city, again_city)
 
-
+"        Основное     "
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply('Привет!\nЯ бот Аби)\nПо команде /help - перечень функция') #, reply_markup=main_keyboard
+    await message.reply('Привет!\nЯ бот Аби)\nПо команде /help - перечень функция', reply_markup=main_keyboard)#
 
 @dp.message_handler(commands=['help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Доступны функции:\nМатематическая игра по команде /start_game\nСожержится ли подстрока в строке /start_sub_string\nПри просьбе дать картинку\n(Например:Привет, кинь, пж, картинку кота)\nотправляет картинку\n(если хотите найти картинку по нескольким словам напишите их через подчеркивание)\n(Картинка ищется через pinterest, иногда может не совпадать с темой)\nОтветы на эти вопросы:\nПривет\nКак дела?\nКакая погода за окном?\nКак тебя зовут?\nСколько тебе дней?\nКоторый час?",)
+    await message.reply("Доступны функции:\nМатематическая игра по команде /start_game\nСожержится ли подстрока в строке /start_sub_string\nПри просьбе дать картинку\n(Например:Привет, кинь, пж, картинку кота)\nотправляет картинку\n(если хотите найти картинку по нескольким словам напишите их через подчеркивание)\n(Картинка ищется через pinterest, иногда может не совпадать с темой)\nИнформация по погоде /start_weather_info\nОтветы на эти вопросы:\nПривет\nКак дела?\nКакая погода за окном?\nКак тебя зовут?\nСколько тебе дней?\nКоторый час?",)
     
-    
-    
-    
+        
+ 
+"       Математическая игра          "   
 @dp.message_handler(commands=['start_game'])
 async def math_game_start(message: types.Message):
     await message.reply('Выберете режим игры', reply_markup=math_game_start_keyboard)
+    await Form.answer_bot.set()     
     
-    
-
-    
-@dp.callback_query_handler()
+@dp.callback_query_handler(state=Form.answer_bot)
 async def inline_buttons(query: types.CallbackQuery):
     global question
     actions = ['-', '+', '*', '/']  
@@ -100,7 +119,22 @@ async def inline_buttons(query: types.CallbackQuery):
         question = f'{randint(1,10)} {actions[randint(0,3)]} {randint(1,10)}'
         await query.message.answer(question)
         await Form.answer_human.set()
-        
+            
+@dp.message_handler(commands=['stop_game'],state=Form.answer_human)
+async def math_game_stop(message: types.Message, state: FSMContext):
+    global counter_true, counter_false, counter_false, counter_questions
+    await message.answer(f'Number of examples: {counter_questions}\nNumber of correct answers: {counter_true}\nNumber of uncorrect answers: {counter_false}', reply_markup=main_keyboard)
+    if counter_true > counter_false:
+        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGv01jkxdn7vbKlIxKhtKhBYUYUNcZoQACCgADnP4yMFsQVnkk-4rwKwQ')
+    elif counter_true < counter_false:
+        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGv09jkxd9GOk0JuebctwhJ4q28uBtJQACGAADnP4yMJPRPDIBxPOGKwQ')
+    else:
+        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGwhljlFHGRmk1awEWbJC4e7ENLx-6JgACCwADnP4yMPctMM3hxWgtKwQ')
+    counter_false = 0
+    counter_questions = 0
+    counter_true = 0
+    await state.finish()
+           
 @dp.callback_query_handler(state=Form.answer_human)
 async def inline_buttons_game(query: types.CallbackQuery):
     global question, counter_true, counter_false, counter_false, counter_questions
@@ -126,24 +160,7 @@ async def inline_buttons_game(query: types.CallbackQuery):
                     math_game_answers_keyboard.add(types.InlineKeyboardButton(text = false_question, callback_data='False'))
                     break
     await query.message.answer(answer_ + str(eval(question)), reply_markup=math_game_answers_keyboard)
-    
 
-@dp.message_handler(commands=['stop_game'],state=Form.answer_human)
-async def math_game_stop(message: types.Message, state: FSMContext):
-    global counter_true, counter_false, counter_false, counter_questions
-    await message.answer(f'Number of examples: {counter_questions}\nNumber of correct answers: {counter_true}\nNumber of uncorrect answers: {counter_false}', reply_markup=types.ReplyKeyboardRemove())
-    if counter_true > counter_false:
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGv01jkxdn7vbKlIxKhtKhBYUYUNcZoQACCgADnP4yMFsQVnkk-4rwKwQ')
-    elif counter_true < counter_false:
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGv09jkxd9GOk0JuebctwhJ4q28uBtJQACGAADnP4yMJPRPDIBxPOGKwQ')
-    else:
-        await bot.send_sticker(message.from_user.id, sticker='CAACAgIAAxkBAAEGwhljlFHGRmk1awEWbJC4e7ENLx-6JgACCwADnP4yMPctMM3hxWgtKwQ')
-    counter_false = 0
-    counter_questions = 0
-    counter_true = 0
-    await state.finish()
-    
-    
 @dp.message_handler(state=Form.answer_human)
 async def math_game_counter(message: types.Message, state: FSMContext):
     global question, counter_true, counter_false, counter_false, counter_questions
@@ -162,22 +179,66 @@ async def math_game_counter(message: types.Message, state: FSMContext):
     
     
     
-    
+"          Подстроки              "
 @dp.message_handler(commands=['start_sub_string'])
-async def math_game_start(message: types.Message):
+async def sub_string_start(message: types.Message):
     await message.reply('Sub string detection is started', reply_markup=sub_string_keyboaed)
     await Form.sub_string_form.set()
 
 @dp.message_handler(commands=['stop_sub_string'],state=Form.sub_string_form)
-async def math_game_stop(message: types.Message, state: FSMContext):
-    await message.reply('Sub string detection is stoped',reply_markup=types.ReplyKeyboardRemove())
+async def sub_string_stop(message: types.Message, state: FSMContext):
+    await message.reply('Sub string detection is stoped', reply_markup=main_keyboard)
     await state.finish()
 
 @dp.message_handler(state=Form.sub_string_form)
-async def math_game_counter(message: types.Message, state: FSMContext):
+async def sub_string_main(message: types.Message, state: FSMContext):
     await message.reply(sub_string(message.text))
 
 
+
+
+"               Погода              "
+@dp.message_handler(commands=['start_weather_info'])
+async def weather_info_startt(message: types.Message):
+    await message.reply('Вітаю у меню інформації о погоді!\nВведіть /start',reply_markup=weather_info_keyboard)
+    await Weather.start.set()
+    
+@dp.message_handler(commands=['stop_weather_info'], state=(Weather.start, Weather.location, Weather.viev))
+async def weather_info_stop(message: types.Message, state: FSMContext):
+    await message.reply('Weather info is stoped', reply_markup=main_keyboard)
+    await state.finish()
+       
+@dp.message_handler(commands=['start'], state=Weather.start)
+async def weather_infoe_start(message: types.Message):
+    await message.answer('Оберіть місто в якому цікавить погода:', reply_markup=weather_start_keyboard)
+    await Weather.location.set()
+
+@dp.callback_query_handler(state=Weather.location)
+async def weather_info_location(query: types.CallbackQuery, state: FSMContext):
+    city_dict = {'Dnipro' : 'Дніпро', 'Kiev' : 'Київ', 'Tours' : 'Тур'}
+    weather_city = getweather(query.data)
+    answer_weather = f'У місті {city_dict[query.data]} {weather_city[0]}, температура {weather_city[1]}°C'
+    await query.message.edit_text(answer_weather, reply_markup=weather_start_keyboard)
+    async with state.proxy() as data:
+        data['answer'] = answer_weather
+
+@dp.message_handler(commands=['start'], state=(Weather.location, Weather.viev))
+async def weather_info_end(message: types.Message):
+    await message.reply('Оберіть що ви хочете зробити:', reply_markup=weather_end_keyboard)
+    await Weather.viev.set()
+    
+@dp.callback_query_handler(state=Weather.viev)
+async def weather_end(query: types.CallbackQuery, state: FSMContext):
+    if query.data == '1':
+        await query.message.edit_text('Оберіть місто в якому цікавить погода:',reply_markup=weather_start_keyboard)
+        await Weather.location.set()
+    elif query.data == '0':
+        async with state.proxy() as data:
+            await query.message.edit_text(data['answer'], reply_markup= types.InlineKeyboardMarkup().add(again_city))
+    
+            
+
+    
 
 
 
@@ -215,7 +276,7 @@ async def echo(message: types.Message):
         await message.answer(bot_answers['answer_time.txt'][randint(0,len(bot_answers['answer_time.txt']) - 1)])
     else:
         await message.answer(message.text)
-        # open(mode='r+', file = 'ответы.txt', encoding='utf-8').write(f'{message.from}')
+        # open(mode='r+', file = 'ответы.txt', encoding='utf-8').write(f'{message.from} : {message.text}\n')
 
 
 if __name__ == '__main__':
